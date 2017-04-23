0f7823cd1b13daced8ba4fa0505a7fc08f87af43
Hsieh11 <Chrishsieh@live.com>
2017-04-23 18:32:47 -0400

    Removed node global typings. We use @Types node module auto installed w npm install

--- server/controllers/BasicController.ts
+++ server/controllers/BasicController.ts
@@ -10,13 +10,6 @@ class BasicController {
     test(req: express.Request, res: express.Response): void {
         try {
 
-            console.log("Attempting google request...");
-            request('http://www.google.com', function (error, response, body) {
-              console.log('error:', error); // Print the error if one occurred
-              console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-              console.log('body:', body); // Print the HTML for the Google homepage.
-            });
-
             console.log("Attempting java backend request...");
             request('https://league-analyzer-backend.herokuapp.com/health', function (error, response, body) {
               console.log('error:', error); // Print the error if one occurred
