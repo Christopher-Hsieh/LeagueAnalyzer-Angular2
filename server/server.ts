f748cc9084738768f8563f2991fd05de0774cb7e
Hsieh11 <Chrishsieh@live.com>
2017-04-24 23:35:49 -0400

    added app.set port

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,8 @@ import BaseRoutes = require("./routes/Routes");
 var port: number = process.env.PORT || 3000;
 var app = express();
 
+app.set('port', port);
+
 app.use('/app', express.static(path.resolve(__dirname, '../client/app')));
 app.use('/libs', express.static(path.resolve(__dirname, '../client/libs')));
 
@@ -23,11 +25,5 @@ app.use('/api', new BaseRoutes().routes);
 app.get('/', function (req, res) {
   res.send('GET request to the homepage')
 })
-
-var server = app.listen(port, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('This express app is listening on port:' + port);
-});
 
 export { app }
